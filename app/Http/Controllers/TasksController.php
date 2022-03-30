445c57462f8cc1d56df800debca936302c33f0aa
a-s <ayn-calla@outlook.com>
2022-03-30 01:45:33 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -53,8 +53,17 @@ class TasksController extends Controller
     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
+        
         //タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -111,9 +120,19 @@ class TasksController extends Controller
     // putまたはpatchでtasks/（任意のid）にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         //タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
